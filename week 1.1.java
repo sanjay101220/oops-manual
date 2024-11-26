7e178e124fb056b98ec88ca1be53f80f6ce66745
sanjay <231001180@rajalakshmi.edu.in>
2024-11-26 10:36:42 +0530

    Create week 1.1.java

---
+++ week 1.1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+public class oddorEven{ 
+public static void 
+main(String[]args){ Scanner s=new 
+Scanner(System.in); int number = 
+s.nextInt(); if(number %2==0){ 
+System.out.println(1); 
+} else 
+{ 
+System.out.println(2); 
+} 
+} 
+}
