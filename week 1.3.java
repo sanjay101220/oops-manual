b24aecb8bb1d66cbd33f9b92b78f0005e5235a8c
sanjay <231001180@rajalakshmi.edu.in>
2024-11-26 10:39:19 +0530

    Create week 1.3.java

---
+++ week 1.3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+import java.lang.Math; 
+public class number{ public static void 
+main(String[]args){ Scanner s= new 
+Scanner(System.in); 
+int a = s.nextInt(); 
+int b = s.nextInt(); 
+System.out.println(Math.abs(a)%10+Math.abs(b)%10); 
+} 
+} 
